a23f89e0b234be6414e14a00dec480e0c5766d13
ogneyar <ya13th@mail.ru>
2023-07-19 14:26:40 +0300

    #autocommit

--- USART1_NoLibs/defines.h
+++ USART1_NoLibs/defines.h
@@ -11,6 +11,33 @@ typedef uint32_t  u32;
 typedef uint16_t  u16;
 typedef uint8_t   u8;
 
+// memory mapped structure for Program Fast Interrupt Controller (PFIC)
+typedef struct{
+    __I  uint32_t ISR[8];
+    __I  uint32_t IPR[8];
+    __IO uint32_t ITHRESDR;
+    __IO uint32_t RESERVED;
+    __IO uint32_t CFGR;
+    __I  uint32_t GISR;
+    __IO uint8_t VTFIDR[4];
+    uint8_t RESERVED0[12];
+    __IO uint32_t VTFADDR[4];
+    uint8_t RESERVED1[0x90];
+    __O  uint32_t IENR[8];
+    uint8_t RESERVED2[0x60];
+    __O  uint32_t IRER[8];
+    uint8_t RESERVED3[0x60];
+    __O  uint32_t IPSR[8];
+    uint8_t RESERVED4[0x60];
+    __O  uint32_t IPRR[8];
+    uint8_t RESERVED5[0x60];
+    __IO uint32_t IACTR[8];
+    uint8_t RESERVED6[0xE0];
+    __IO uint8_t IPRIOR[256];
+    uint8_t RESERVED7[0x810];
+    __IO uint32_t SCTLR;
+}PFIC_Type;
+
 // memory mapped structure for SysTick
 typedef struct
 {
@@ -140,6 +167,29 @@ typedef struct
     uint16_t  RESERVED9;
 } SPI_TypeDef;
 
+// External Interrupt/Event Controller
+typedef struct
+{
+    __IO uint32_t INTENR; 
+    __IO uint32_t EVENR;   
+    __IO uint32_t RTENR;   
+    __IO uint32_t FTENR;   
+    __IO uint32_t SWIEVR;  
+    __IO uint32_t INTFR;   
+} EXTI_TypeDef;
+
+// Alternate Function I/O
+typedef struct
+{
+    __IO uint32_t ECR;
+    __IO uint32_t PCFR1;
+    __IO uint32_t EXTICR[4];
+    uint32_t RESERVED0;
+    __IO uint32_t PCFR2;  
+} AFIO_TypeDef;
+
+
+
 #define SET         1
 #define RESET       0
 
@@ -150,19 +200,36 @@ typedef struct
 #define LED_G       4 // PB4
 #define LED_B       5 // PB5
 
-#define PA2         2 // 
+#define PA0         0 // 
+#define PA1         1 // 
+#define PA2         2 // USR_BTN
 #define PA3         3 // 
 #define PA4         4 // DAC
 #define PA5         5 // SPI1 CLK
 #define PA6         6 // SPI1 MISO
 #define PA7         7 // SPI1 MOSI
+#define PA8         8 // 
 #define PA9         9 // Tx1
+#define PA10       10 // 
+#define PA11       11 // 
+#define PA12       12 // 
+#define PA13       13 // 
+#define PA14       14 // 
+#define PA15       15 // 
 
 #define PB3         3 // LED_R
 #define PB4         4 // LED_G
 #define PB5         5 // LED_B
+#define PB6         6 // 
+#define PB7         7 // 
+#define PB8         8 // 
+#define PB9         9 // 
 #define PB10       10 // SCK I2C2
 #define PB11       11 // SDA I2C2
+#define PB12       12 // 
+#define PB13       13 // 
+#define PB14       14 // 
+#define PB15       15 // 
 
 #define GPIO_Msk        0b1111
 #define GPIO_Speed_50   0b0011
@@ -186,6 +253,8 @@ typedef struct
 #define I2C2_BASE              (APB1PERIPH_BASE + 0x5800)   // 0x40005800
 #define DAC_BASE               (APB1PERIPH_BASE + 0x7400)   // 0x40007400
 
+#define AFIO_BASE              (APB2PERIPH_BASE + 0x0000)   // 0x40010000
+#define EXTI_BASE              (APB2PERIPH_BASE + 0x0400)   // 0x40010400
 #define GPIOA_BASE             (APB2PERIPH_BASE + 0x0800)   // 0x40010800
 #define GPIOB_BASE             (APB2PERIPH_BASE + 0x0C00)   // 0x40010C00
 #define SPI1_BASE              (APB2PERIPH_BASE + 0x3000)   // 0x40013000
@@ -194,11 +263,17 @@ typedef struct
 #define RCC_BASE               (AHBPERIPH_BASE + 0x1000)    // 0x40021000
 #define EXTEN_BASE             (AHBPERIPH_BASE + 0x3800)    // 0x40023800
 
+
+#define PFIC                   ((PFIC_Type *) 0xE000E000 )
+#define NVIC                   PFIC
+
 #define SysTick                ((SysTick_Type *) 0xE000F000)
 
 #define I2C2                   ((I2C_TypeDef *) I2C2_BASE)
 #define DAC                    ((DAC_TypeDef *) DAC_BASE)
 
+#define AFIO                   ((AFIO_TypeDef *) AFIO_BASE)
+#define EXTI                   ((EXTI_TypeDef *) EXTI_BASE)
 #define GPIOA                  ((GPIO_TypeDef *) GPIOA_BASE)
 #define GPIOB                  ((GPIO_TypeDef *) GPIOB_BASE)
 #define SPI1                   ((SPI_TypeDef *) SPI1_BASE)
@@ -209,6 +284,7 @@ typedef struct
 #define RCC_APB1PCENR_I2C2EN   ((uint32_t)0x00400000) // (1 << 22)
 #define RCC_APB1PCENR_DACEN    ((uint32_t)0x20000000) // (1 << 29)
 
+#define RCC_APB2PCENR_AFIOEN   ((uint32_t)0x00000001) // (1 << 0)
 #define RCC_APB2PCENR_IOPAEN   ((uint32_t)0x00000004) // (1 << 2)
 #define RCC_APB2PCENR_IOPBEN   ((uint32_t)0x00000008) // (1 << 3)
 #define RCC_APB2PCENR_SPI1EN   ((uint32_t)0x00001000) // (1 << 12)
@@ -240,6 +316,13 @@ typedef struct
 #define SYSCLK_FREQ_144MHz_HSI 144000000
 
 
+           
+/*
+
+USART
+
+*/
+
 #define CTLR1_UE_Set              ((uint16_t)0x2000) // USART Enable Mask
 #define CTLR1_UE_Reset            ((uint16_t)0xDFFF) // USART Disable Mask
 
@@ -247,12 +330,6 @@ typedef struct
 #define CTLR1_CLEAR_Mask          ((uint16_t)0xE9F3) // USART CR1 Mask
 #define CTLR3_CLEAR_Mask          ((uint16_t)0xFCFF) // USART CR3 Mask
 
-           
-/*
-
-USART
-
-*/
    
 // USART_Word_Length
 #define USART_WordLength_8b       ((uint16_t)0x0000)
@@ -365,6 +442,91 @@ DAC
 
 /*
 
+EXTI
+
+*/
+
+// EXTI mode enumeration
+typedef enum
+{
+    EXTI_Mode_Interrupt = 0x00,
+    EXTI_Mode_Event = 0x04
+}EXTIMode_TypeDef;
+
+// EXTI Trigger enumeration
+typedef enum
+{
+    EXTI_Trigger_Rising = 0x08,
+    EXTI_Trigger_Falling = 0x0C,  
+    EXTI_Trigger_Rising_Falling = 0x10
+}EXTITrigger_TypeDef;
+
+// EXTI_Lines 
+#define EXTI_Line0       ((uint32_t)0x00001)  /* External interrupt line 0 */
+#define EXTI_Line1       ((uint32_t)0x00002)  /* External interrupt line 1 */
+#define EXTI_Line2       ((uint32_t)0x00004)  /* External interrupt line 2 */
+#define EXTI_Line3       ((uint32_t)0x00008)  /* External interrupt line 3 */
+#define EXTI_Line4       ((uint32_t)0x00010)  /* External interrupt line 4 */
+#define EXTI_Line5       ((uint32_t)0x00020)  /* External interrupt line 5 */
+#define EXTI_Line6       ((uint32_t)0x00040)  /* External interrupt line 6 */
+#define EXTI_Line7       ((uint32_t)0x00080)  /* External interrupt line 7 */
+#define EXTI_Line8       ((uint32_t)0x00100)  /* External interrupt line 8 */
+#define EXTI_Line9       ((uint32_t)0x00200)  /* External interrupt line 9 */
+#define EXTI_Line10      ((uint32_t)0x00400)  /* External interrupt line 10 */
+#define EXTI_Line11      ((uint32_t)0x00800)  /* External interrupt line 11 */
+#define EXTI_Line12      ((uint32_t)0x01000)  /* External interrupt line 12 */
+#define EXTI_Line13      ((uint32_t)0x02000)  /* External interrupt line 13 */
+#define EXTI_Line14      ((uint32_t)0x04000)  /* External interrupt line 14 */
+#define EXTI_Line15      ((uint32_t)0x08000)  /* External interrupt line 15 */
+#define EXTI_Line16      ((uint32_t)0x10000)  /* External interrupt line 16 Connected to the PVD Output */
+#define EXTI_Line17      ((uint32_t)0x20000)  /* External interrupt line 17 Connected to the RTC Alarm event */
+#define EXTI_Line18      ((uint32_t)0x40000)  /* External interrupt line 18 Connected to the USBD/USBFS OTG Wakeup from suspend event */                                    
+#define EXTI_Line19      ((uint32_t)0x80000)  /* External interrupt line 19 Connected to the Ethernet Wakeup event */
+#define EXTI_Line20      ((uint32_t)0x100000) /* External interrupt line 20 Connected to the USBHS Wakeup event */
+
+
+
+
+
+
+/*
+
+GPIO
+
+*/
+
+// GPIO_Port_Sources
+#define GPIO_PortSourceGPIOA        ((uint8_t)0x00)
+#define GPIO_PortSourceGPIOB        ((uint8_t)0x01)
+#define GPIO_PortSourceGPIOC        ((uint8_t)0x02)
+#define GPIO_PortSourceGPIOD        ((uint8_t)0x03)
+#define GPIO_PortSourceGPIOE        ((uint8_t)0x04)
+#define GPIO_PortSourceGPIOF        ((uint8_t)0x05)
+#define GPIO_PortSourceGPIOG        ((uint8_t)0x06)
+
+// GPIO_Pin_sources
+#define GPIO_PinSource0             ((uint8_t)0x00)
+#define GPIO_PinSource1             ((uint8_t)0x01)
+#define GPIO_PinSource2             ((uint8_t)0x02)
+#define GPIO_PinSource3             ((uint8_t)0x03)
+#define GPIO_PinSource4             ((uint8_t)0x04)
+#define GPIO_PinSource5             ((uint8_t)0x05)
+#define GPIO_PinSource6             ((uint8_t)0x06)
+#define GPIO_PinSource7             ((uint8_t)0x07)
+#define GPIO_PinSource8             ((uint8_t)0x08)
+#define GPIO_PinSource9             ((uint8_t)0x09)
+#define GPIO_PinSource10            ((uint8_t)0x0A)
+#define GPIO_PinSource11            ((uint8_t)0x0B)
+#define GPIO_PinSource12            ((uint8_t)0x0C)
+#define GPIO_PinSource13            ((uint8_t)0x0D)
+#define GPIO_PinSource14            ((uint8_t)0x0E)
+#define GPIO_PinSource15            ((uint8_t)0x0F)
+
+
+
+
+/*
+
 I2C
 
 */
@@ -428,6 +590,89 @@ I2C
 
 // I2C FLAG mask
 #define FLAG_Mask                ((uint32_t)0x00FFFFFF)
+
+
+
+/*
+
+NVIC
+
+*/
+
+// Preemption_Priority_Group
+#define NVIC_PriorityGroup_0           ((uint32_t)0x00)
+#define NVIC_PriorityGroup_1           ((uint32_t)0x01)
+#define NVIC_PriorityGroup_2           ((uint32_t)0x02)
+#define NVIC_PriorityGroup_3           ((uint32_t)0x03)
+#define NVIC_PriorityGroup_4           ((uint32_t)0x04)
+
+
+
+
+
+/*
+
+SPI
+
+*/
+
+// SPI or I2S mode selection masks
+#define SPI_Mode_Select       ((uint16_t)0xF7FF)
+#define I2S_Mode_Select       ((uint16_t)0x0800)
+
+// SPI SPE mask
+#define CTLR1_SPE_Set         ((uint16_t)0x0040)
+#define CTLR1_SPE_Reset       ((uint16_t)0xFFBF)
+
+// SPI_data_direction  
+#define SPI_Direction_2Lines_FullDuplex ((uint16_t)0x0000)
+#define SPI_Direction_2Lines_RxOnly     ((uint16_t)0x0400)
+#define SPI_Direction_1Line_Rx          ((uint16_t)0x8000)
+#define SPI_Direction_1Line_Tx          ((uint16_t)0xC000)
+
+// SPI_mode
+#define SPI_Mode_Master                 ((uint16_t)0x0104)
+#define SPI_Mode_Slave                  ((uint16_t)0x0000)
+
+// SPI_data_size
+#define SPI_DataSize_16b                ((uint16_t)0x0800)
+#define SPI_DataSize_8b                 ((uint16_t)0x0000)
+
+// SPI_Clock_Polarity
+#define SPI_CPOL_Low                    ((uint16_t)0x0000)
+#define SPI_CPOL_High                   ((uint16_t)0x0002)
+
+// SPI_Clock_Phase
+#define SPI_CPHA_1Edge                  ((uint16_t)0x0000)
+#define SPI_CPHA_2Edge                  ((uint16_t)0x0001)
+
+// SPI_Slave_Select_management
+#define SPI_NSS_Soft                    ((uint16_t)0x0200)
+#define SPI_NSS_Hard                    ((uint16_t)0x0000)
+
+// SPI_BaudRate_Prescaler
+#define SPI_BaudRatePrescaler_2         ((uint16_t)0x0000)
+#define SPI_BaudRatePrescaler_4         ((uint16_t)0x0008)
+#define SPI_BaudRatePrescaler_8         ((uint16_t)0x0010)
+#define SPI_BaudRatePrescaler_16        ((uint16_t)0x0018)
+#define SPI_BaudRatePrescaler_32        ((uint16_t)0x0020)
+#define SPI_BaudRatePrescaler_64        ((uint16_t)0x0028)
+#define SPI_BaudRatePrescaler_128       ((uint16_t)0x0030)
+#define SPI_BaudRatePrescaler_256       ((uint16_t)0x0038)
+
+// SPI_MSB_LSB_transmission
+#define SPI_FirstBit_MSB                ((uint16_t)0x0000)
+#define SPI_FirstBit_LSB                ((uint16_t)0x0080)
+
+// SPI_I2S_flags_definition
+#define SPI_I2S_FLAG_RXNE               ((uint16_t)0x0001)
+#define SPI_I2S_FLAG_TXE                ((uint16_t)0x0002)
+#define I2S_FLAG_CHSIDE                 ((uint16_t)0x0004)
+#define I2S_FLAG_UDR                    ((uint16_t)0x0008)
+#define SPI_FLAG_CRCERR                 ((uint16_t)0x0010)
+#define SPI_FLAG_MODF                   ((uint16_t)0x0020)
+#define SPI_I2S_FLAG_OVR                ((uint16_t)0x0040)
+#define SPI_I2S_FLAG_BSY                ((uint16_t)0x0080)
 
 
 #endif /* __DEFINES_H_ */
